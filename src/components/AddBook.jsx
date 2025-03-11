87fc65f6352b2d50336c6640a0f8b6d201d4a6d1
Adetayo <twinebravo@gmail.com>
2025-03-11 23:05:00 +0100

    Added React JS File

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -136,15 +136,6 @@ const AddBook = () => {
                       onChange={handleChange}
                       required
                     />
-                    <input
-                      type="text"
-                      name="bookPrice"
-                      className="form-control"
-                      placeholder="Book Price"
-                      value={formData.bookPrice === 0 ? "" : formData.bookPrice}
-                      onChange={handleChange}
-                      required
-                    />
                     
                     <CustomButton
                       className="btn btn-secondary"
